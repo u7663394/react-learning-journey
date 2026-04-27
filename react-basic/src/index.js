d2e44fbce64e44cff2d501a5dd5e794618d7a9b2
u7663394 <u7663394@anu.edu.au>
2026-04-28 09:30:47 +1000

    feat: rules for updating state

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -172,3 +172,76 @@ root.render(
     <Counter />
   </div>,
 );
+
+/**
+ * 修改状态的规则: 不要直接修改状态变量, 而是创建新值
+ * 传递给 setState 函数来更新状态
+ */
+
+const ModifyState = () => {
+  const [count, setCount] = useState(10);
+  const [list, setList] = useState(["Apple", "Banana", "Cherry"]);
+  const [user, setUser] = useState({ name: "Alice", age: 25 });
+
+  return (
+    <div>
+      <h3>简单类型</h3>
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          // count += 1;
+          setCount(count + 1);
+        }}
+      >
+        修改
+      </button>
+
+      <hr />
+
+      <h3>数组</h3>
+      <div>{list.join(", ")}</div>
+      <button
+        onClick={() => {
+          // list.push("Orange");
+          setList([...list, "Orange"]);
+        }}
+      >
+        添加
+      </button>
+      <button
+        onClick={() => {
+          setList(list.filter((ele) => ele !== "Banana"));
+        }}
+      >
+        删除
+      </button>
+      <button
+        onClick={() => {
+          setList(list.map((ele) => (ele === "Banana" ? "Grape" : ele)));
+        }}
+      >
+        修改
+      </button>
+
+      <hr />
+
+      <h3>对象</h3>
+      <div>
+        {user.name} - {user.age}
+      </div>
+      <button
+        onClick={() => {
+          setUser({ ...user, name: "Bob" });
+        }}
+      >
+        修改
+      </button>
+    </div>
+  );
+};
+
+root.render(
+  <div>
+    <ModifyState />
+  </div>,
+);
