802e4087afcb7729c53519b97eb96986933ccddc
u7663394 <u7663394@anu.edu.au>
2026-04-28 10:27:02 +1000

    feat: implement comment list rendering for `Bilibili case`

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,8 +1,55 @@
+import { useState } from "react";
 import "./App.scss";
 // 使用本地图片需要先导入
 import avatar from "./assets/avatar.svg";
 
+// 评论列表数据
+const defaultList = [
+  {
+    rpid: 3,
+    user: {
+      uid: "13258165",
+      avatar: "",
+      uname: "周杰伦",
+    },
+    content: "哎哟，不错哦",
+    ctime: "10-18 08:15",
+    like: 68,
+  },
+  {
+    rpid: 2,
+    user: {
+      uid: "36080105",
+      avatar: "",
+      uname: "许嵩",
+    },
+    content: "我寻你千百度 日出到迟暮",
+    ctime: "11-13 11:29",
+    like: 66,
+  },
+  {
+    rpid: 1,
+    user: {
+      uid: "30009257",
+      avatar,
+      uname: "React Learner",
+    },
+    content: "Learn React step by step",
+    ctime: "10-19 09:00",
+    like: 99,
+  },
+];
+
+// 用户信息
+const user = {
+  uid: "30009257",
+  avatar,
+  uname: "React Learner",
+};
+
 const App = () => {
+  const [list, setList] = useState(defaultList);
+
   return (
     <div className="app">
       {/* 导航 Tab */}
@@ -11,7 +58,7 @@ const App = () => {
           <li className="nav-title">
             <span className="nav-title-text">评论</span>
             {/* 评论数量 */}
-            <span className="total-reply">{10}</span>
+            <span className="total-reply">{list.length}</span>
           </li>
           <li className="nav-sort">
             {/* 高亮类名： active */}
@@ -46,39 +93,50 @@ const App = () => {
         {/* 评论列表 */}
         <div className="reply-list">
           {/* 评论项 */}
-          <div className="reply-item">
-            {/* 头像 */}
-            <div className="root-reply-avatar">
-              <div className="bili-avatar">
-                <img
-                  className="bili-avatar-img"
-                  alt="avatar"
-                  src="https://yjy-teach-oss-cn-beijing.aliyuncs.com/react-basic/demo.png"
-                />
-              </div>
-            </div>
+          {list.map((item) => {
+            return (
+              <div key={item.rpid} className="reply-item">
+                {/* 头像 */}
+                <div className="root-reply-avatar">
+                  <div className="bili-avatar">
+                    <img
+                      className="bili-avatar-img"
+                      alt="avatar"
+                      src={item.user.avatar || avatar}
+                    />
+                  </div>
+                </div>
 
-            <div className="content-wrap">
-              {/* 用户名 */}
-              <div className="user-info">
-                <div className="user-name">Jack</div>
-              </div>
-              {/* 评论内容 */}
-              <div className="root-reply">
-                <span className="reply-content">这是一条评论回复</span>
-                <div className="reply-info">
-                  {/* 评论时间 */}
-                  <span className="reply-time">{"2023-11-11"}</span>
-                  {/* 评论数量 */}
-                  <span className="reply-time">点赞数:{100}</span>
-                  <span className="delete-btn">删除</span>
+                <div className="content-wrap">
+                  {/* 用户名 */}
+                  <div className="user-info">
+                    <div className="user-name">{item.user.uname}</div>
+                  </div>
+                  {/* 评论内容 */}
+                  <div className="root-reply">
+                    <span className="reply-content">{item.content}</span>
+                    <div className="reply-info">
+                      {/* 评论时间 */}
+                      <span className="reply-time">{item.ctime}</span>
+                      {/* 喜欢 */}
+                      <span className="reply-like">
+                        <button>like</button>
+                        <span>{item.like}</span>
+                      </span>
+                      {/* 不喜欢 */}
+                      <span className="reply-dislike">
+                        <button className="icon icon-dislike">dislike</button>
+                      </span>
+                      <span className="delete-btn">删除</span>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
 
-        <div className="reply-none">暂无评论</div>
+        {list.length === 0 && <div className="reply-none">暂无评论</div>}
       </div>
     </div>
   );
