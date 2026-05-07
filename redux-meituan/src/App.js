b69729a14f44a5ab503843fdee2730ce41c88cf5
u7663394 <u7663394@anu.edu.au>
2026-05-07 14:43:14 +1000

    feat: implement food list switching display for Meituan case

--- redux-meituan/src/App.js
+++ redux-meituan/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   /**
    * 渲染
    */
-  const { foodsList } = useSelector((state) => state.foods);
+  const { foodsList, activeIndex } = useSelector((state) => state.foods);
   return (
     <div className="home">
       {/* 导航 */}
@@ -33,15 +33,17 @@ const App = () => {
           <div className="list-content">
             <div className="goods-list">
               {/* 外卖商品列表 */}
-              {foodsList.map((item) => {
+              {foodsList.map((item, index) => {
                 return (
-                  <FoodsCategory
-                    key={item.tag}
-                    // 列表标题
-                    name={item.name}
-                    // 列表商品
-                    foods={item.foods}
-                  />
+                  index === activeIndex && (
+                    <FoodsCategory
+                      key={item.tag}
+                      // 列表标题
+                      name={item.name}
+                      // 列表商品
+                      foods={item.foods}
+                    />
+                  )
                 );
               })}
             </div>
