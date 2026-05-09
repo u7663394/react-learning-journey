8c4ab31fc2078b65623e77b5f27fdce4a52a0069
u7663394 <u7663394@anu.edu.au>
2026-05-09 13:01:59 +1000

    feat: quickly set up basic routing

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -1,5 +1,24 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+// 路由
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// 1. 创建路由对象
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>首页</h1>,
+  },
+  {
+    path: "/login",
+    element: <h1>登录页面</h1>,
+  },
+]);
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
