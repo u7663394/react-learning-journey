981237d9092d7100d2e28e306e6a022fd0d500fc
u7663394 <u7663394@anu.edu.au>
2026-05-09 15:09:08 +1000

    feat: implement route navigation and redirection

--- react-router/src/page/Home/index.js
+++ react-router/src/page/Home/index.js
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Home 首页</h1>
+      {/* 命令式导航: 使用 useNavigate 钩子进行编程式跳转 */}
+      <button onClick={() => navigate("/login")}>跳转到登陆页</button>
     </div>
   );
 };
--- react-router/src/page/Login/index.js
+++ react-router/src/page/Login/index.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div>
       <h1>Login 登录</h1>
+      {/* 声明式导航: 在 Link 组件中添加 to 属性指定跳转路径 */}
+      <Link to="/">跳转到首页</Link>
     </div>
   );
 };
