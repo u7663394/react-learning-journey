f3c89d40a9b449a0c4e90abe2271a61030422ea5
u7663394 <u7663394@anu.edu.au>
2026-05-11 00:21:16 +1000

    style: customize Ant Design theme colors

--- react-bill-case/src/index.js
+++ react-bill-case/src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import "./theme.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
--- react-bill-case/src/pages/Layout/index.js
+++ react-bill-case/src/pages/Layout/index.js
@@ -1,9 +1,11 @@
+import { Button } from "antd-mobile";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
       <h1>Layout</h1>
+      <Button color="primary">测试全局</Button>
       <Outlet></Outlet>
     </div>
   );
