b85799a3370ed1916feabc78faae3c7c4f35b27e
u7663394 <u7663394@anu.edu.au>
2026-05-11 21:16:41 +1000

    feat: implement TabBar functionality

--- react-bill-case/src/pages/Layout/index.js
+++ react-bill-case/src/pages/Layout/index.js
@@ -1,8 +1,33 @@
-import { Button } from "antd-mobile";
+import { TabBar } from "antd-mobile";
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { fetchBillList } from "@/store/modules/billStore";
+import "./index.scss";
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+
+// tab 栏数据
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
 
 const Layout = () => {
   /**
@@ -12,11 +37,27 @@ const Layout = () => {
   useEffect(() => {
     dispatch(fetchBillList());
   }, [dispatch]);
+
+  /**
+   * 切换路由
+   */
+  const navigate = useNavigate();
+  const switchRoute = (path) => {
+    navigate(path);
+  };
+
   return (
-    <div>
-      <h1>Layout</h1>
-      <Button color="primary">测试全局</Button>
-      <Outlet></Outlet>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={switchRoute}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   );
 };
