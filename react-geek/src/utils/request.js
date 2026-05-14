33222624bbd85251cf4ce40030c3269a75de3188
u7663394 <u7663394@anu.edu.au>
2026-05-14 12:13:24 +1000

    refactor: encapsulate request module for geek project

---
+++ react-geek/src/utils/request.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+// 封装 axios 实例
+const request = axios.create({
+  baseURL: "http://geek.itheima.net/v1_0",
+  timeout: 10000,
+});
+
+// 请求拦截器
+request.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// 响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default request;
