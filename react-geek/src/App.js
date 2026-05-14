bef1c838534ac8d73e19ff008eb09cd4e0e1d83c
u7663394 <u7663394@anu.edu.au>
2026-05-14 10:12:07 +1000

    chore: install Sass and Ant Design packages

--- react-geek/src/App.js
+++ react-geek/src/App.js
@@ -1,7 +1,10 @@
+import { Button } from "antd";
+
 function App() {
   return (
     <div className="App">
       <h1>React Geek</h1>
+      <Button type="primary">Test</Button>
     </div>
   );
 }
