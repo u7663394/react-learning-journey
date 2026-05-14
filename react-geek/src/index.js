92fd20df4ae8d4d12fb8d90f25323d835cd69834
u7663394 <u7663394@anu.edu.au>
2026-05-14 12:25:15 +1000

    feat: add Redux boilerplate for token management

--- react-geek/src/index.js
+++ react-geek/src/index.js
@@ -2,6 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
