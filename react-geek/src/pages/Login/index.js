ad560afb209c49dd2fab4842b91c23212bfe06c7
u7663394 <u7663394@anu.edu.au>
2026-05-14 10:56:26 +1000

    feat: complete static layout and style for login page

--- react-geek/src/pages/Login/index.js
+++ react-geek/src/pages/Login/index.js
@@ -1,7 +1,27 @@
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/logo.png";
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form validateTrigger="onBlur">
+          <Form.Item name="mobile">
+            <Input size="large" placeholder="Please enter your mobile number" />
+          </Form.Item>
+          <Form.Item name="code">
+            <Input size="large" placeholder="Please enter verification code" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 };
