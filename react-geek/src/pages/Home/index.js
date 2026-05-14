e5510972cfeb1ade1d4474a33522a3e0b356fe1e
u7663394 <u7663394@anu.edu.au>
2026-05-14 18:57:15 +1000

    feat: prepare basic ECharts on Home page

--- react-geek/src/pages/Home/index.js
+++ react-geek/src/pages/Home/index.js
@@ -1,7 +1,33 @@
+import * as echarts from "echarts";
+import { useEffect } from "react";
+
 const Home = () => {
+  useEffect(() => {
+    const chartDom = document.getElementById("main");
+    const myChart = echarts.init(chartDom);
+    const option = {
+      title: {
+        text: "ECharts Demo",
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["vue", "React", "Angular"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "Percentage",
+          type: "bar",
+          data: [35, 45, 20],
+        },
+      ],
+    };
+    myChart.setOption(option);
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <div id="main" style={{ width: "500px", height: "400px" }}></div>
     </div>
   );
 };
