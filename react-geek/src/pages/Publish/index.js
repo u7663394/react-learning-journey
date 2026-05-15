5e4e51544ac7961b1c5949b3b7dd9c20cdef22ae
u7663394 <u7663394@anu.edu.au>
2026-05-15 21:35:48 +1000

    feat: complete basic layout for Publish page

--- react-geek/src/pages/Publish/index.js
+++ react-geek/src/pages/Publish/index.js
@@ -1,7 +1,82 @@
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  Input,
+  Upload,
+  Space,
+  Select,
+  message,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import "./index.scss";
+
 const Publish = () => {
   return (
-    <div>
-      <h1>Publish</h1>
+    <div className="publish">
+      <Card
+        title={
+          <Breadcrumb
+            items={[
+              { title: <Link to={"/"}>首页</Link> },
+              { title: "发布文章" },
+            ]}
+          />
+        }
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 0 }}
+        >
+          <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: "请输入文章标题" }]}
+          >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item
+            label="频道"
+            name="channel_id"
+            rules={[{ required: true, message: "请选择文章频道" }]}
+          >
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+              {/* value属性用户选中之后会自动收集起来作为接口的提交字段 */}
+            </Select>
+          </Form.Item>
+          <Form.Item label="封面">
+            <Form.Item name="type">
+              <Radio.Group>
+                <Radio value={1}>单图</Radio>
+                <Radio value={3}>三图</Radio>
+                <Radio value={0}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            {/* 
+              listType: 决定选择文件框的外观样式
+              showUploadList: 控制显示上传列表
+            */}
+          </Form.Item>
+          <Form.Item
+            label="内容"
+            name="content"
+            rules={[{ required: true, message: "请输入文章内容" }]}
+          >
+            {/* 富文本编辑器 */}
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size="large" type="primary" htmlType="submit">
+                发布文章
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 };
