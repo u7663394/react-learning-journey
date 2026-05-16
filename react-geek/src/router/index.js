5b948f5c86757f5ace740d8324a4ea7ab631b2f8
u7663394 <u7663394@anu.edu.au>
2026-05-16 22:13:19 +1000

    perf: optimize bundle size with route lazy loading

--- react-geek/src/router/index.js
+++ react-geek/src/router/index.js
@@ -2,19 +2,53 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 import AuthRoute from "@/components/AuthRoute";
-import Home from "@/pages/Home";
-import Article from "@/pages/Article";
-import Publish from "@/pages/Publish";
+import { lazy, Suspense } from "react";
 
+const Home = lazy(() => import("@/pages/Home"));
+const Article = lazy(() => import("@/pages/Article"));
+const Publish = lazy(() => import("@/pages/Publish"));
+
+/**
+ * Suspense 提供一个 "等待期间的占位内容"
+ * 组件没加载完时, 会先显示 fallback 中的内容
+ */
 const router = createBrowserRouter([
   {
     path: "/",
     element: AuthRoute(<Layout />),
     children: [
-      { index: true, element: <Home /> },
-      { path: "home", element: <Home /> },
-      { path: "article", element: <Article /> },
-      { path: "publish", element: <Publish /> },
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: "home",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: "article",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Article />
+          </Suspense>
+        ),
+      },
+      {
+        path: "publish",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Publish />
+          </Suspense>
+        ),
+      },
     ],
   },
   {
