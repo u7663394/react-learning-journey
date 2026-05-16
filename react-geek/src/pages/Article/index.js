4b6f7a9107e3ecd26423639f07ef7aef38f72cc0
u7663394 <u7663394@anu.edu.au>
2026-05-16 10:49:40 +1000

    feat: build static layout for Article manage page

--- react-geek/src/pages/Article/index.js
+++ react-geek/src/pages/Article/index.js
@@ -1,7 +1,153 @@
+import { Link } from "react-router-dom";
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  DatePicker,
+  Select,
+  Popconfirm,
+  Table,
+  Tag,
+  Space,
+} from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import img404 from "@/assets/error.png";
+
 const Article = () => {
+  const status = {
+    1: <Tag color="warning">Pending Review</Tag>,
+    2: <Tag color="success">Review Approved</Tag>,
+  };
+
+  const columns = [
+    {
+      title: "Cover",
+      dataIndex: "cover",
+      width: 120,
+      render: (cover) => {
+        return (
+          <img src={cover.images[0] || img404} width={80} height={60} alt="" />
+        );
+      },
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      width: 220,
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      // data - 后端返回的状态status 根据它做条件渲染
+      // data === 1 => 待审核
+      // data === 2 => 审核通过
+      render: (data) => status[data],
+    },
+    {
+      title: "Publish Time",
+      dataIndex: "pubdate",
+    },
+    {
+      title: "Read Count",
+      dataIndex: "read_count",
+    },
+    {
+      title: "Comment Count",
+      dataIndex: "comment_count",
+    },
+    {
+      title: "Like Count",
+      dataIndex: "like_count",
+    },
+    {
+      title: "Actions",
+      render: (data) => {
+        return (
+          <Space size="middle">
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<EditOutlined />}
+              // onClick={() => navigate(`/publish?id=${data.id}`)}
+            />
+            <Popconfirm
+              title="Delete Article"
+              description="Are you sure you want to delete the current article?"
+              // onConfirm={() => onConfirm(data)}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button
+                type="primary"
+                danger
+                shape="circle"
+                icon={<DeleteOutlined />}
+              />
+            </Popconfirm>
+          </Space>
+        );
+      },
+    },
+  ];
+
   return (
     <div>
-      <h1>Article</h1>
+      <Card
+        title={
+          <Breadcrumb
+            items={[
+              { title: <Link to={"/"}>Home</Link> },
+              { title: "Article List" },
+            ]}
+          />
+        }
+        style={{ marginBottom: 20 }}
+      >
+        <Form initialValues={{ status: "" }}>
+          <Form.Item label="Status" name="status">
+            <Radio.Group>
+              <Radio value={""}>All</Radio>
+              <Radio value={1}>Pending Review</Radio>
+              <Radio value={2}>Review Approved</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="Channel" name="channel_id">
+            <Select placeholder="Select article channel" style={{ width: 200 }}>
+              {/* {channelList.map((item) => (
+                <Select.Option key={item.id} value={item.id}>
+                  {item.name}
+                </Select.Option>
+              ))} */}
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="Date" name="date">
+            <DatePicker.RangePicker />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ marginLeft: 40 }}>
+              Filter
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+      {/* 表格区域 */}
+      <Card title={`Got 1 result based on the filter conditions:`}>
+        <Table
+          rowKey="id"
+          columns={columns}
+          // dataSource={list}
+          pagination={{
+            total: 1,
+            // pageSize: reqData.per_page,
+            // onChange: onPageChange,
+          }}
+        />
+      </Card>
     </div>
   );
 };
