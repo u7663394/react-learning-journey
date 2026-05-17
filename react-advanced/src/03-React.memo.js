0a8c2133d8ce65aa2513db53c062e693376333e4
u7663394 <u7663394@anu.edu.au>
2026-05-17 15:52:09 +1000

    feat: prop comparison mechanism in React.memo

--- react-advanced/src/03-React.memo.js
+++ react-advanced/src/03-React.memo.js
@@ -5,9 +5,15 @@ import { memo, useState } from "react";
  * 作用: 允许组件在 Props 没有改变的情况下跳过重新渲染
  * 默认渲染机制: 父组件重新渲染时，子组件也会重新渲染
  * 语法: const newComponent = React.memo(Component)
+ * 比较机制:
+ *   - 对每一个 prop 使用 Object.is 进行比较 (引用是否变化)
+ *   - Object.is(7,7) => true
+ *   - Object.is('abc','abc') => true
+ *   - Object.is([],[]) => false
+ *   - Object.is({a:1},{a:1}) => false
  */
 
-const Son = (props) => {
+const Son = ({ count }) => {
   console.log("Son组件重新渲染了");
   return <div>Son组件</div>;
 };
@@ -22,8 +28,9 @@ const App = () => {
       <button onClick={() => setCount(count + 1)}>
         点击增加count: {count}
       </button>
+      <button onClick={() => setCount(0)}>重置count: {count}</button>
       {/* <Son /> */}
-      <MemoSon />
+      <MemoSon count={count} />
     </div>
   );
 };
